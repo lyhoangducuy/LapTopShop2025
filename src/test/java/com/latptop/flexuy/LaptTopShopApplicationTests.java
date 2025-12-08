727bf5fe627d5675e4bd46395603cabf11369c0b
lyhoangducuy <lyhoangducuy@gmail.com>
2025-12-08 16:47:58 +0700

    first commit

---
+++ src/test/java/com/latptop/flexuy/LaptTopShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.latptop.flexuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptTopShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
